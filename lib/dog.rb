1746cd6439ba0925790f13363bb0ed5b635c8789
Jordan Yee <jordanyee799@gmail.com>
2021-11-23 09:26:30 -0800

    commit tree 5ffc7677dca0accbb7f2c1c22a6f2cbe467e1958

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,16 @@
 class Dog
+    def name
+        @name
+    end
 
+    def name=(input)
+        @name = input
+    end
+    def breed
+        @breed
+    end
+
+    def breed=(input)
+        @breed = input
+    end
 end
